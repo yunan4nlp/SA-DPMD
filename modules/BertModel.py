537b037fafb939b63a59bde63a1cad9b004cd715
Nan Yu <yunan.hlju@gmail.com>
2021-10-21 16:27:41 +0800

    update lib

--- modules/BertModel.py
+++ modules/BertModel.py
@@ -1,17 +1,12 @@
-from transformers.modeling_bert import *
+from transformers.models.bert.modeling_bert import *
 from modules.Layer import *
-
-
-class MyBertModel(BertModel):
-    def __init__(self, config):
-        super(MyBertModel, self).__init__(config)
 
 
 class BertExtractor(nn.Module):
     def __init__(self, config, tok_helper):
         super(BertExtractor, self).__init__()
         self.config = config
-        self.bert = MyBertModel.from_pretrained(config.bert_dir)
+        self.bert = BertModel.from_pretrained(config.bert_dir)
         self.bert.resize_token_embeddings(len(tok_helper.tokenizer))
         print("Load bert model finished.")
 
@@ -22,8 +17,8 @@ class BertExtractor(nn.Module):
         self.layer_num = self.end_layer - self.start_layer
 
     def forward(self, input_ids, token_type_ids, attention_mask):
-        sequence_output, pooled_output, encoder_outputs = self.bert(input_ids=input_ids,
-                                                                    attention_mask=attention_mask,
-                                                                    token_type_ids=token_type_ids,
-                                                                    output_hidden_states=True)
-        return sequence_output, pooled_output, encoder_outputs
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            output_hidden_states=True)
+        return outputs.last_hidden_state, outputs.pooler_output, outputs.hidden_states
